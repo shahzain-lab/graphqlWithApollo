6fa4d9823874fc36c64ac2b21f4a594d911f6637
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-01 13:54:58 -0800

    connecting with server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 ///css
 import './App.css';
+////Apollo Provider
+//setup clints
+import {ApolloClient, InMemoryCache} from '@apolloClient';
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
