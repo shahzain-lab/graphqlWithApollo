6dc523884053adf79e381ee3e278f78a5ea49545
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-01 18:17:46 -0800

    adding footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
 
-export const Footer = () => {
-    return (
-        <div>
-            
+
+export const Footer =()=> {
+    return(
+        <div className="footer-div">
+
+          
+          <h3 className="footerhead">Developer: <span>@Shahzain</span></h3> 
+          <a href="www.github.com/shahzain-lab/apolloclient" className="github">Github code</a>
+        
         </div>
     )
 }
