779a538b6cad420dd8d00e1e804865b366eb1eda
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-01 18:18:56 -0800

    adding header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export const Header = () => {
     return (
+        <div className="main-div">
         <div>
-            
+            <h3 className="head">Top 5 developing countries</h3>
+            <p className="para">Developing countries tend to have some characteristics in common. For example, with regards to health risks, they commonly have: low levels of access to safe drinking water, sanitation and hygiene; energy poverty; high levels of pollution (e.g. air pollution, indoor air pollution, water pollution); high proportion of people with tropical and infectious diseases (neglected tropical diseases); a high number of road traffic accidents; and generally poor infrastructure. </p>
         </div>
+        <img src="https://www.researchgate.net/profile/Pim_Martens/publication/40891422/figure/fig3/AS:669566233505795@1536648438262/Map-of-the-KOF-Index-of-Globalisation-2009_Q640.jpg"/>
+    </div>
     )
 }
